7dd2a4a09ee6f554ddc6f3281c17ec189c0774a7
Mark Elvers <mark.elvers@tunbury.org>
2022-11-21 22:18:11 +0000

    Added cmdliner

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# sqlite3-backup
+
+Backup large SQLite databases
+
+```shell
+# sqlite3-backup /var/lib/docker/volumes/ocaml-ci_data/_data/var/db/sqlite.db /var/lib/docker/volumes/ocaml-ci_data/_data/var/db/backup.db
+Source database is 92360004 KB plus 115106 KB WAL
+Backing up 23090002 pages from /var/lib/docker/volumes/ocaml-ci_data/_data/var/db/sqlite.db
+Backup up 23090002 pages to /var/lib/docker/volumes/ocaml-ci_data/_data/var/db/backup.db in 170.028848 seconds
+```
+
--- bin/main.ml
+++ bin/main.ml
@@ -1,76 +1,100 @@
-
-let t_src = "sqlite.db"
-let t_dst = "backup-sqlite.db"
+open Sqlite3
+open Cmdliner
 
 let or_fail label x =
   match x with
-  | Sqlite3.Rc.OK -> ()
-  | err -> Fmt.failwith "Sqlite3 %s error: %s" label (Sqlite3.Rc.to_string err)
+  | Rc.OK -> ()
+  | err -> Fmt.failwith "Sqlite3 %s error: %s" label (Rc.to_string err)
 
-let src = Sqlite3.db_open t_src
+let sqlite_backup t_src t_dst =
 
-let () = List.iter ( fun sql -> Sqlite3.exec src sql |> or_fail sql )
-[ "PRAGMA busy_timeout=10000"; "PRAGMA temp_store=MEMORY"; ]
+  let src = db_open t_src in
 
-let journal_mode = 
-  let query_stmt = Sqlite3.prepare src "PRAGMA journal_mode" in
-    let () = Sqlite3.reset query_stmt |> or_fail "reset query" in
-    match Sqlite3.step query_stmt with
-      | Sqlite3.Rc.ROW -> Sqlite3.Data.to_string_exn (Sqlite3.column query_stmt 0)
-      | err -> Fmt.failwith "Sqlite3 page_size error: %s" (Sqlite3.Rc.to_string err)
+  let () = List.iter ( fun sql -> exec src sql |> or_fail sql )
+  [ "PRAGMA busy_timeout=10000"; "PRAGMA temp_store=MEMORY"; ] in
 
-let page_size = 
-  let query_stmt = Sqlite3.prepare src "PRAGMA page_size" in
-    let () = Sqlite3.reset query_stmt |> or_fail "reset query" in
-    match Sqlite3.step query_stmt with
-      | Sqlite3.Rc.ROW -> Sqlite3.Data.to_int_exn (Sqlite3.column query_stmt 0)
-      | err -> Fmt.failwith "Sqlite3 page_size error: %s" (Sqlite3.Rc.to_string err)
+  let journal_mode =
+    let query_stmt = prepare src "PRAGMA journal_mode" in
+      let () = reset query_stmt |> or_fail "reset query" in
+      match step query_stmt with
+        | Rc.ROW -> Data.to_string_exn (column query_stmt 0)
+        | err -> Fmt.failwith "Sqlite3 page_size error: %s" (Rc.to_string err) in
 
-let () = let st = Unix.stat t_src in
-  Fmt.pr "Source database is %i KB" (st.st_size / 1024)
+  let page_size =
+    let query_stmt = prepare src "PRAGMA page_size" in
+      let () = reset query_stmt |> or_fail "reset query" in
+      match step query_stmt with
+        | Rc.ROW -> Data.to_int_exn (column query_stmt 0)
+        | err -> Fmt.failwith "Sqlite3 page_size error: %s" (Rc.to_string err) in
 
-let _ = 
-  match journal_mode with
-    | "wal" ->
-      let st = Unix.stat (t_src ^ "-wal") in
-        let wal_pages = (min st.st_size (1024 * 1024 * 1024)) / page_size in
-          if wal_pages > 2000 then begin
-            let sql = "PRAGMA cache_size=" ^ (string_of_int wal_pages) in
-              Sqlite3.exec src sql |> or_fail sql
-          end ;
-       Fmt.pr " plus %i KB WAL\n" (st.st_size / 1024)
-    | _ -> Fmt.pr "\n"
+  let () = let st = Unix.stat t_src in
+    Fmt.pr "Source database is %i KB" (st.st_size / 1024) in
 
-let dst = Sqlite3.db_open t_dst
+  let () =
+    match journal_mode with
+      | "wal" ->
+        let st = Unix.stat (t_src ^ "-wal") in
+          let wal_pages = (min st.st_size (1024 * 1024 * 1024)) / page_size in
+            if wal_pages > 2000 then begin
+              let sql = "PRAGMA cache_size=" ^ (string_of_int wal_pages) in
+                exec src sql |> or_fail sql
+            end ;
+         Fmt.pr " plus %i KB WAL\n" (st.st_size / 1024)
+      | _ -> Fmt.pr "\n" in
 
-let () = List.iter ( fun sql -> Sqlite3.exec dst sql |> or_fail sql )
-[ "PRAGMA synchronous=OFF"; "PRAGMA journal_mode=TRUNCATE"; "PRAGMA temp_store=MEMORY"; "PRAGMA cache_size=1"; ]
+  let dst = db_open t_dst in
 
-let backup = (Sqlite3.Backup.init ~dst ~dst_name:"main" ~src ~src_name:"main")
+  let () = List.iter ( fun sql -> exec dst sql |> or_fail sql )
+  [ "PRAGMA synchronous=OFF"; "PRAGMA journal_mode=TRUNCATE"; "PRAGMA temp_store=MEMORY"; "PRAGMA cache_size=1"; ] in
 
-let () = (Sqlite3.Backup.step backup 0 |> or_fail "backup step")
+  let backup = (Backup.init ~dst ~dst_name:"main" ~src ~src_name:"main") in
 
-let () = Fmt.pr "Backing up %i pages from %s\n" (Sqlite3.Backup.remaining backup) t_src
+  let () = (Backup.step backup 0 |> or_fail "backup step") in
 
-let start = Unix.gettimeofday ()
+  let () = Fmt.pr "Backing up %i pages from %s\n" (Backup.remaining backup) t_src in
 
-let () = 
-  let rec run () = match (Sqlite3.Backup.step backup (-1)) with
-    | Sqlite3.Rc.LOCKED -> Unix.sleep 10; run ()
-    | Sqlite3.Rc.BUSY -> run ()
-    | Sqlite3.Rc.DONE -> ()
-    | err -> Fmt.pr "Sqlite3 error: %s" (Sqlite3.Rc.to_string err)
-    in run ()
+  let start = Unix.gettimeofday () in
 
-let stop= Unix.gettimeofday ()
+  let () =
+    let rec run () = match (Backup.step backup (-1)) with
+      | Rc.LOCKED -> Unix.sleep 10; run ()
+      | Rc.BUSY -> run ()
+      | Rc.DONE -> ()
+      | err -> Fmt.pr "Sqlite3 error: %s" (Rc.to_string err)
+      in run () in
 
-let () = Fmt.pr "Backup up %i pages to %s in %f seconds\n" (Sqlite3.Backup.pagecount backup) t_dst (stop -. start)
+  let stop = Unix.gettimeofday () in
 
-let () = (Sqlite3.Backup.finish backup |> or_fail "backup finished")
+  let () = Fmt.pr "Backup up %i pages to %s in %f seconds\n" (Backup.pagecount backup) t_dst (stop -. start) in
 
-let () = Sqlite3.exec dst ("PRAGMA journal_mode=" ^ journal_mode) |> or_fail "failed to set journal_mode"
+  let () = (Backup.finish backup |> or_fail "backup finished") in
 
-let () = if not (Sqlite3.db_close src && Sqlite3.db_close dst) then
-  Fmt.failwith "Sqlite3 close error"
+  let () = exec dst ("PRAGMA journal_mode=" ^ journal_mode) |> or_fail "failed to set journal_mode" in
 
+  let () = if not (db_close src && db_close dst) then
+    Fmt.failwith "Sqlite3 close error" in
+
+  ()
+
+let source =
+  let doc = "Source database file to backup." in
+  Arg.(required & pos 0 (some string) None & info [] ~docv:"SOURCE" ~doc)
+
+let destination =
+  let doc = "Database file to backup to." in
+  Arg.(required & pos 1 (some string) None & info [] ~docv:"DESTINATION" ~doc)
+
+let sqlite_backup_t = Term.(const sqlite_backup $ source $ destination)
+
+let cmd =
+  let doc = "Backup a SQLite database." in
+  let man = [
+    `S Manpage.s_bugs;
+    `P "Email bug reports to <mark@tarides.com>." ]
+  in
+  let info = Cmd.info "sqlite3-backup" ~version:"%%VERSION%%" ~doc ~man in
+  Cmd.v info sqlite_backup_t
+
+let main () = exit (Cmd.eval cmd)
+let () = main ()
 
